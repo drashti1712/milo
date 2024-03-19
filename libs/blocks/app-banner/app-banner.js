5893291e056a03233314e47fff3593e447708215
Drashti Modasara <drashti@drashtis-mbp.corp.adobe.com>
2024-03-19 12:24:59 +0530

    branch-banner cleaned code

--- libs/blocks/app-banner/app-banner.js
+++ libs/blocks/app-banner/app-banner.js
@@ -1,8 +1,6 @@
 import branchInit from "../../deps/branch-io.js";
 
 export default function init(el) {
-  console.log(el);
   const header = document.querySelector('.global-navigation');
-  console.log(header);
   branchInit(header);
 }
